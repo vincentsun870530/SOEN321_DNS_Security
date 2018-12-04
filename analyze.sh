4370c716903b148ebac0bc82ea9ae3bc9bacb18c
Guillaume Ah-ki <guillaumeahki@gmail.com>
2018-12-04 03:13:51 -0500

    renamed file

--- analyze.sh
+++ analyze.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-printf > domains_ip.txt;
-echo These domains are blocked: >> domains_ip.txt;
+printf > blocked_domains.txt;
+echo These domains are blocked: >> blocked_domains.txt;
 numOfDomains=0;
 numOfDomainsBlocked=0;
 for i in `cat domains.txt`; 
@@ -14,7 +14,7 @@ redirect=$(echo "$addressLookupOutput" | grep "name = " | awk '{print $4}');
 if [ "$redirect" == "hit-adult.opendns.com." ]
 then
   let "numOfDomainsBlocked++";
-  echo "$domainName" >> domains_ip.txt;
+  echo "$domainName" >> blocked_domains.txt;
 fi
 done
-printf "%d/%d domains got blocked" "$numOfDomainsBlocked" "$numOfDomains" >> domains_ip.txt;
+printf "%d/%d domains got blocked" "$numOfDomainsBlocked" "$numOfDomains" >> blocked_domains.txt;

5bccd23c20e566ba2b503f1b89c6d8ecc239ff28
Guillaume Ah-ki <guillaumeahki@gmail.com>
2018-12-04 13:20:28 -0500

    added yandex redirect url in if statement

--- analyze.sh
+++ analyze.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-printf > nortonPornBlacklist.txt;
-echo These domains are blocked: >> nortonPornBlacklist.txt;
+printf > yandexPornBlacklist.txt;
+echo These domains are blocked: >> yandexPornBlacklist.txt;
 numOfDomains=0;
 numOfDomainsBlocked=0;
-for i in `cat filteredPornDomains.txt`; 
+for i in `cat testPornDomains.txt`; 
 do 
 let "numOfDomains++";
 domainLookupOutput=$(nslookup $i);
@@ -11,10 +11,10 @@ domainName=$(echo "$domainLookupOutput" | grep ^Name | awk '{print $2}');
 domainAddress=$(echo "$domainLookupOutput" | grep ^Address | awk '/Address/{i++}i==2{print $2}');
 addressLookupOutput=$(nslookup "$domainAddress");
 redirect=$(echo "$addressLookupOutput" | grep "name = " | awk '{print $4}');
-if [ "$redirect" == "hit-adult.opendns.com." ]
+if [ "$redirect" == "hit-adult.opendns.com." ] || [ "$redirect" == "safe2.yandex.ru." ]
 then
   let "numOfDomainsBlocked++";
-  echo "$domainName" >> nortonPornBlacklist.txt;
+  echo "$domainName" >> yandexPornBlacklist.txt;
 fi
 done
-printf "%d/%d domains got blocked" "$numOfDomainsBlocked" "$numOfDomains" >> nortonPornBlacklist.txt;
+printf "%d/%d domains got blocked" "$numOfDomainsBlocked" "$numOfDomains" >> yandexPornBlacklist.txt;
